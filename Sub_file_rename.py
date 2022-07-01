f4765740a13026dca050f1c654c1acb3d26a4feb
alex-cardenas <35149352+alex-cardenas@users.noreply.github.com>
2022-07-01 12:42:22 -0500

    Copy and Paste
    Using shutil.py, copy and paste are working

--- Sub_file_rename.py
+++ Sub_file_rename.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 import os
 
 def subtitles(path_str):
@@ -11,11 +12,14 @@ def subtitles(path_str):
     for file_path in path.iterdir():
         if file_path.name == "Subs":
             for sub_file in file_path.iterdir():
-                print("old subtitle file name is: ", sub_file.name)
-                sub_file.rename(path.name)
-                print("new subtitle file name is: ", sub_file.name)
-
+                print("subtitle file path is: ", sub_file)
+                print("subtitle file name is: ", sub_file.name)
+                shutil.copy2(sub_file, path)
 
 if __name__ == "__main__":
     path_str = input("Enter path:\n")
     subtitles(path_str)
+
+# print("old subtitle file name is: ", sub_file.name)
+# sub_file.rename(path.name)
+# print("new subtitle file name is: ", sub_file.name)
