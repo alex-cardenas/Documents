50ad12c6ca2292bab1b39f0aeef6b88d3a15317a
alex-cardenas <35149352+alex-cardenas@users.noreply.github.com>
2022-07-01 12:52:21 -0500

    Should be working now
    Added descriptive notes

--- Sub_file_rename.py
+++ Sub_file_rename.py
@@ -3,11 +3,15 @@ import shutil
 import os
 
 def subtitles(path_str):
+    #Change working directory to user input path
     os.chdir(path_str)
+    #Set path_file to the new working directory
     path_file = os.getcwd()
     path = Path(path_file)
     print("working directory name is: ", path)
     print("file name should be: ", path.name)
+    #initialises variable for later use
+    old_sub_name = ""
 
     for file_path in path.iterdir():
         if file_path.name == "Subs":
@@ -15,11 +19,11 @@ def subtitles(path_str):
                 print("subtitle file path is: ", sub_file)
                 print("subtitle file name is: ", sub_file.name)
                 shutil.copy2(sub_file, path)
+                #Allows os.rename to find the newly copied file
+                old_sub_name = sub_file.name
+
+    os.rename(old_sub_name, path.name + ".srt")
 
 if __name__ == "__main__":
     path_str = input("Enter path:\n")
     subtitles(path_str)
-
-# print("old subtitle file name is: ", sub_file.name)
-# sub_file.rename(path.name)
-# print("new subtitle file name is: ", sub_file.name)
